5f6dd83ab8b560c4c87a7539c8002dd964960cbe
komikoni <komikoni@gmail.com>
2020-12-09 15:49:20 +0900

    サンプルにAWS Blog系を追加 翻訳機能を追加
    ただ、大量処理の場合のリミット制御はできてない。

--- src/index.js
+++ src/index.js
@@ -1,35 +1,39 @@
 /**
  * A Lambda function that logs the payload received from a CloudWatch scheduled event.
  */
+const AWS = require('aws-sdk');
+const Translate = new AWS.Translate();
+
 const pLimit = require('p-limit');
 const Parser = require('rss-parser');
 const parser = new Parser();
 const { IncomingWebhook } = require('ms-teams-webhook');
 const dayjs = require('dayjs');
 require('dayjs/locale/ja');
-dayjs.locale('ja')
 const utc = require('dayjs/plugin/utc'); // dependent on utc plugin
 const timezone = require('dayjs/plugin/timezone');
 const localizedFormat = require('dayjs/plugin/localizedFormat');
 const isSameOrAfter = require('dayjs/plugin/isSameOrAfter');
+dayjs.locale('ja')
 dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.extend(localizedFormat);
 dayjs.extend(isSameOrAfter);
 
 exports.handler = async (event, context, callback) => {
-    // console.info(JSON.stringify(event));
+    console.info(JSON.stringify(event));
 
     const teamsWebHookUrl = process.env.TEAMS_WEBHOOK_URL;
     const webhook = new IncomingWebhook(teamsWebHookUrl);
     const teamsWebHookLimit = Number(process.env.TEAMS_WEBHOOK_LIMIT);
     const executionIntervalHour = Number(process.env.EXECUTION_INTERVAL_HOUR);
-    const limit = pLimit(teamsWebHookLimit);
+    const webHookLimit = pLimit(teamsWebHookLimit);
+    const translateLimit = pLimit(2);
 
     const rssList = JSON.parse(process.env.RSS_LIST);
     await Promise.all(rssList.map(async rss => {
         const feed = await parser.parseURL(rss.Url);
-        console.log(JSON.stringify(feed));
+        // console.log(JSON.stringify(feed));
         await Promise.all(feed.items.filter(item => {
             const targetTime = dayjs(event.time).subtract(executionIntervalHour, 'hour');
             // console.log(`targetTime=${targetTime.format()}`);
@@ -46,14 +50,51 @@ exports.handler = async (event, context, callback) => {
                 "title": item.title,
                 "sections": [
                     {
-                        "activityTitle": `<a href=${item.link}>${displayTime}</a>`,
+                        "activityTitle": `<a href=${item.link}>${rss.title}: ${displayTime}</a>`,
                         "activitySubtitle": item.categories.join(','),
                         "text": item.content,
                         "markdown": false // html mode
                     }
+                ],
+                "potentialAction": [
+                    {
+                        "@type": "OpenUri",
+                        "name": "記事を読む",
+                        "targets": [
+                            {
+                                "os": "default",
+                                "uri": item.link
+                            }
+                        ]
+                    }
                 ]
             };
-            return await limit(() => webhook.send(JSON.stringify(sendBody)));
+            if (rss.translation) {
+                const titleParams = {
+                    Text: item.title,
+                    SourceLanguageCode: 'en',
+                    TargetLanguageCode: 'ja',
+                }
+                const contentParams = {
+                    Text: item.contentSnippet,
+                    SourceLanguageCode: 'en',
+                    TargetLanguageCode: 'ja',
+                }
+                // const jaTitle = await translateLimit(() => Translate.translateText(titleParams).promise());
+                // const jaContent = await translateLimit(() => Translate.translateText(contentParams).promise());
+                // const jaTitle = item.title;//await  Translate.translateText(titleParams).promise();
+                // const jaContent = item.contentSnippet;//await Translate.translateText(contentParams).promise();
+                const jaTitle = await Translate.translateText(titleParams).promise();
+                const jaContent = await Translate.translateText(contentParams).promise();
+
+                sendBody.sections.push({
+                    "activityTitle": jaTitle.TranslatedText,
+                    "activitySubtitle": "",
+                    "text": jaContent.TranslatedText,
+                    "markdown": false // html mode
+                })
+            }
+            return await webHookLimit(() => webhook.send(JSON.stringify(sendBody)));
         }));
     }));
     callback(null, 'Success');
